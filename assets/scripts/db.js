e45733a63d6a2223f4fa1dc1cbe55b29ca94c8dc
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-06-14 11:28:27 -0400

    user input

---
+++ assets/scripts/db.js
@@ -0,0 +1,16 @@
+const musics = [
+    {
+      songName: 'love of my life',
+      songArtist: 'queen',
+      hint1: '../musics/loveofmylife-hint2.m4a',
+      hint2: '../musics/loveofmylife-hint3.m4a',
+      hint3: 'One of the most desired feelings in the world + The opposite of death',
+    },
+    {
+      songName: 'candy shop',
+      songArtist: '50 cent',
+      hint1: '',
+      hint2: '',
+      hint3: 'Treat that every child likes + Action of spending money',
+    }
+];
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,27 +1,52 @@
-const musics = [
-    {
-      songName: 'love of my life',
-      songArtist: 'queen',
-      hint1: '../musics/loveofmylife-hint2.m4a',
-      hint2: '../musics/loveofmylife-hint3.m4a',
-      hint3: 'One of the most desired feelings in the world + The opposite of death',
-    },
-    {
-      songName: 'candy shop',
-      songArtist: '50 cent',
-      hint1: '',
-      hint2: '',
-      hint3: 'Treat that every child likes + Action of spending money',
-    }
-];
+// const musics = require("./db")
 
-const playMusicButton = document.getElementById("play-hint1")
-function playMusic(event) {
-    For (let i = 0; i < musics.length; i++) {
-        Audio.play.hint1(musics)
-    }
+// todos os documents
+const playhint1Button=document.getElementById("play-hint1")
+const playhint2Button=document.getElementById("play-hint2")
+const playhint3Button=document.getElementById("play-hint3")
+
+const outputAnswer=document.getElementById("outputAnswer")
+const userInputButton=document.getElementById("userInputButton")
+
+const audio1=new Audio("./assets/musics/loveofmylife-hint2.m4a")
+const audio2=new Audio("./assets/musics/loveofmylife-hint3.m4a")
+
+
+//  todos os events
+playhint1Button.addEventListener('click', () => {
+  audio1.play()
+}) 
+playhint2Button.addEventListener('click', () => {
+  audio2.play()
+}) 
+playhint3Button.addEventListener('click', () => {
+  // console.log('testando')
+  return displayHint3();
+}) 
+
+function displayHint3(){
+  document.getElementById("hint3").innerHTML = "One of the most desired feelings in the world + The opposite of death";
+}   
+
+let song1='love of my life'
+
+userInputButton.addEventListener('click', () => {
+  let input=document.getElementById("userInput").value;
+  if(input===song1) {
+    outputAnswer.innerText=`You guessed right, the song is ${input}`
+  }
+  else {
+    (outputAnswer.innerText="Try it again!")}
+}) 
+
+// function playMusic(event) {
+//     for (let i = 0; i < musics.length; i++) {
+//         Audio.play.hint1(musics)
+//     }
     
 
-}
+// }
 
-document.addEventListener('click', playMusic)
+// alert("cliquei no botao")
+//   console.log("cliquei no botao")
+
