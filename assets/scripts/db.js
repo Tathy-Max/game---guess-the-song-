644ed573d292a2eebe93a2a5c0df9ab9cb099224
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-06-14 16:07:17 -0400

    start button first attempt

--- assets/scripts/db.js
+++ assets/scripts/db.js
@@ -1,16 +1,23 @@
 const musics = [
     {
-      songName: 'love of my life',
+      songName: 'LOVE OF MY LIFE',
       songArtist: 'queen',
-      hint1: '../musics/loveofmylife-hint2.m4a',
-      hint2: '../musics/loveofmylife-hint3.m4a',
+      hint1: '../musics/loveofmylife-hint1.m4a',
+      hint2: '../musics/loveofmylife-hint2.m4a',
       hint3: 'One of the most desired feelings in the world + The opposite of death',
     },
     {
-      songName: 'candy shop',
+      songName: 'CANDY SHOP',
       songArtist: '50 cent',
-      hint1: '',
-      hint2: '',
+      hint1: '../musics/candyshop-hint1.m4a',
+      hint2: '../musics/candyshop-hint2.m4a',
       hint3: 'Treat that every child likes + Action of spending money',
-    }
+    },
+    {
+        songName: 'GAROTA DE IPANEMA',
+        songArtist: 'tom jobim',
+        hint1: '../musics/garotadeipanema-hint1.m4a',
+        hint2: '../musics/garotadeipanema-hint2.m4a',
+        hint3: 'The opposite of boy + Beautiful beach in Brazil',
+      }
 ];
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,11 @@
-// const musics = require("./db")
+//puxando a minha array de musicas
+const arrMusics = musics
+console.log(musics);
 
 // todos os documents
+const gameStartButton=document.getElementById("game-start-button")
+const gameBoard = document.getElementById("game-div");
+
 const playhint1Button=document.getElementById("play-hint1")
 const playhint2Button=document.getElementById("play-hint2")
 const playhint3Button=document.getElementById("play-hint3")
@@ -8,11 +13,26 @@ const playhint3Button=document.getElementById("play-hint3")
 const outputAnswer=document.getElementById("outputAnswer")
 const userInputButton=document.getElementById("userInputButton")
 
-const audio1=new Audio("./assets/musics/loveofmylife-hint2.m4a")
-const audio2=new Audio("./assets/musics/loveofmylife-hint3.m4a")
+//todas as intancias
+// const game=new Game(gameBoard)
 
+const audio1=new Audio("./assets/musics/loveofmylife-hint1.m4a")
+const audio2=new Audio("./assets/musics/loveofmylife-hint2.m4a")
 
 //  todos os events
+gameStartButton.addEventListener('click', () => {
+  game.showGameSection()
+})  
+
+function showGameSection() {
+  game.style="none";
+}    
+
+function showGameSection() {
+  document.getElementById("game-div").style.display;
+  
+}  
+
 playhint1Button.addEventListener('click', () => {
   audio1.play()
 }) 
@@ -26,18 +46,24 @@ playhint3Button.addEventListener('click', () => {
 
 function displayHint3(){
   document.getElementById("hint3").innerHTML = "One of the most desired feelings in the world + The opposite of death";
-}   
+}
 
-let song1='love of my life'
+let song1='LOVE OF MY LIFE'
 
 userInputButton.addEventListener('click', () => {
   let input=document.getElementById("userInput").value;
-  if(input===song1) {
-    outputAnswer.innerText=`You guessed right, the song is ${input}`
+  if(input.toUpperCase()===song1) {
+    outputAnswer.innerText=`You guessed right, the song is ${song1}`
   }
   else {
     (outputAnswer.innerText="Try it again!")}
 }) 
+
+
+
+
+
+
 
 // function playMusic(event) {
 //     for (let i = 0; i < musics.length; i++) {
@@ -48,5 +74,4 @@ userInputButton.addEventListener('click', () => {
 // }
 
 // alert("cliquei no botao")
-//   console.log("cliquei no botao")
-
+//   console.log("cliquei no botao") 
