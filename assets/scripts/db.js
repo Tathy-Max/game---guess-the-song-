84533f84e01109e22242a111f4be1aa6904cc8c7
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-06-15 22:32:09 -0400

    db updated

--- assets/scripts/db.js
+++ assets/scripts/db.js
@@ -2,22 +2,33 @@ const musics = [
     {
       songName: 'LOVE OF MY LIFE',
       songArtist: 'queen',
-      hint1: './assets/musics/loveofmylife-hint1.m4a',
-      hint2: './assets/musics/loveofmylife-hint2.m4a',
+      hint1: './assets/musics/loveOfMyLifeHint1.m4a',
+      hint2: './assets/musics/loveOfMyLifeHint2.m4a',
       hint3: 'One of the most desired feelings in the world + The opposite of death',
+      originalMusic: './assets/musics/Love of my life.m4a',
     },
     {
       songName: 'CANDY SHOP',
       songArtist: '50 cent',
-      hint1: './assets/musics/candyshop-hint1.m4a',
-      hint2: './assets/musics/candyshop-hint2.m4a',
+      hint1: './assets/musics/candyShopHint1.m4a',
+      hint2: './assets/musics/candyShopHint2.m4a',
       hint3: 'Treat that every child likes + Action of spending money',
+      originalMusic: './assets/musics/Candy shop.m4a',
     },
     {
-      songName: 'GAROTA DE IPANEMA',
+      songName: 'GIRL FROM IPANEMA',
       songArtist: 'tom jobim',
-      hint1: './assets/musics/garotadeipanema-hint1.m4a',
-      hint2: './assets/musics/garotadeipanema-hint2.m4a',
+      hint1: './assets/musics/girlFromIpanemaHint1.m4a',
+      hint2: './assets/musics/girlFromIpanemaHint2.m4a',
       hint3: 'The opposite of boy + Beautiful beach in Brazil',
-      }
+      originalMusic: './assets/musics/Girl from Ipanema.m4a',
+    },
+    {
+      songName: 'SUGAR',
+      songArtist: 'maroon 5',
+      hint1: './assets/musics/sugarHint1.m4a',
+      hint2: './assets/musics/sugarHint2.m4a',
+      hint3: 'Food that makes things sweet',
+      originalMusic: './assets/musics/Sugar.m4a',
+    }
 ];
