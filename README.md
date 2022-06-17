4fe71e86ff3ed4ca59d5bdfd6e594a0bab0fefe3
Tathyanna Maximiano <tathyanna.maximiano@gmail.com>
2022-06-17 04:08:46 -0400

    readme

---
+++ README.md
@@ -0,0 +1,28 @@
+Guess the music - Game
+
+The Guess the Music is a game built as the first project  of the Ironhack Full Stack Web Development bootcamp
+
+
+# Links
+
+Deploy link -
+Presentation link -
+
+## The mechanics
+
+The game's goal is to guess the song's names throughout hints reaching the end of the game's fourth holding the **maximum score** as possible. An initial score of one hundred points is given to the player at the start game. This score is increased when the player guesses the name of the song, or is decreased each time the player clicks to get a new hint, or misses the name of the song after the third attempt that each round offers. Attempts can be made at any time by typing a guess on the input button. The answer must be sent via the check button to be verified. The check works whether the guess was typed using uppercase or lowercase.
+If the score reaches zero points, the game is over.
+Each round the player will find: a new song to guess, three hints to help and three chances to try a guess.
+Have fun!
+
+## Inspirations
+
+My main inspirations for this project were the passion for music and games, a TV show that I loved to watch in my childhood and the desire to develop my technical skills in the web development field.
+
+## ## Technology Used
+
+JavaScript, HTML and CSS were used to build the game, without additional frameworks. Interactions were created using DOM manipulations. For each round, functions and events are called to iterate through an array that contains all the information for all the songs in the game.
+
+## Special Thanks
+
+My special thanks go to the Ironhack teaching staff who helped me all along the project: Karen Okasaki, Anna Reis, and Alexandre Lima. Also, thanks to all my classmates, especially Tulio Minini.  
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,73 +1,68 @@
-// alldocuments
+// html elements
 const gameStartButton=document.getElementById("game-start-button")
 const initialSection=document.getElementsByClassName("initial-page-section")
 const gameBoardSection=document.getElementsByClassName("game-section")
-const hint=document.getElementById("hint3")
-const userInput=document.getElementById("userInput")
-const score=document.getElementById("score")
-const turn=document.getElementById("turn")
-const gameOverScreen=document.getElementById("gameOver")
-const winScreen=document.getElementById("winScreen")
-
 const playhint1Button=document.getElementById("play-hint1")
 const playhint2Button=document.getElementById("play-hint2")
 const playhint3Button=document.getElementById("play-hint3")
-// const playhint2ButtonHidden=document.getElementsByClassName("play-hint")
-// const playhint3ButtonHidden=document.getElementsByClassName("play-hint")
-const nextSongButton=document.getElementById("nextSong")
-const inputDiv=document.getElementById("input")
-
+const hint=document.getElementById("hint3")
+const userInput=document.getElementById("userInput")
+const userInputButton=document.getElementById("userInputButton") //check button
 const outputAnswer=document.getElementById("outputAnswer")
-const userInputButton=document.getElementById("userInputButton")
-//const backTobeginButton=document.getElementById("backTobegin")
+const nextSongButton=document.getElementById("nextSong")
+const score=document.getElementById("score")
+const turn=document.getElementById("turn")
+const inputDiv=document.getElementById("input")
+const gameOverScreen=document.getElementById("gameOver")
+const winScreen=document.getElementById("winScreen")
 
-// all variables
+// variables
 let round=0
-turn.innerText=(round+1)
 let inputCounter=[];
-let pontos=100
-score.innerText=pontos
-
-// all instances
 let audio3=new Audio(musics[round].hint1)
 let audio4=new Audio(musics[round].hint2)
 let song=musics[round].songName
+let pontos=100
+score.innerText=pontos
+turn.innerText=(round+1)
 
-// const audio1=new Audio("./assets/musics/loveofmylife-hint1.m4a") // hint1
-// const audio2=new Audio("./assets/musics/loveofmylife-hint2.m4a") // hint2
-
-// all functions
+// functions
 function nextSong(index) {
-  console.log(index)
   if (index===4) {
     winGame()
     return
   }
-
   audio3=new Audio(musics[index].hint1)
   audio4=new Audio(musics[index].hint2)
   audioOriginal=new Audio(musics[index].originalMusic)
-  
   song=musics[index].songName
 }
-
-  
-
 function displayHint3(){
   hint.innerText=musics[round].hint3
 }
-
-// all events
+function reset () { //clean up for next round
+  inputCounter=[]
+  outputAnswer.innerText=""
+  userInput.value=""
+  userInputButton.hidden=false
+}
+function gameOver() {
+  inputDiv.classList.add("hidden")
+  gameOverScreen.classList.remove("hidden")
+}
+function winGame() {
+  inputDiv.classList.add("hidden")
+  winScreen.classList.remove("hidden")
+}
+// eventListeners
 gameStartButton.addEventListener('click', () => {
   initialSection[0].classList.remove("show")
   initialSection[0].classList.add("hidden")
-
   gameBoardSection[0].classList.remove("hidden")
   gameBoardSection[0].classList.add("show")
   
   nextSong(round)
 })  
-
 playhint1Button.addEventListener('click', () => {
   audio3.play();
   playhint2Button.classList.remove("hidden")
@@ -87,45 +82,30 @@ playhint3Button.addEventListener('click', () => {
   score.innerText=pontos
   return displayHint3();
 }) 
-
-
-nextSongButton.addEventListener('click', () => { //eventlistner do botao nextsong
-  console.log("clicou no botao next 1")
+nextSongButton.addEventListener('click', () => { //eventListner nextsong button
   audioOriginal.pause() 
   reset()     
   nextSong(round+=1)
   turn.innerText=(round+1)
-  hint.innerText=""
-
-  console.log(round)
-  
+  hint.innerText=""  
+  playhint2Button.classList.add("hidden")
+  playhint2Button.classList.remove("show")
+  playhint3Button.classList.add("hidden")
+  playhint3Button.classList.remove("show")
 })
-
-
-
-
-
-// eventlistner do botao check
-userInputButton.addEventListener('click', () => {
-  
-
-
-
-
-
+userInputButton.addEventListener('click', () => { //eventListener check button
   let input=document.getElementById("userInput").value;
   inputCounter.push(input)
   
-  if (inputCounter.length<=3 && input.toUpperCase() === song) { //if se a resposta esta correta
+  if (inputCounter.length<=3 && input.toUpperCase() === song) { //if condition for right answer
     outputAnswer.innerText=`You rock! The song is ${song}`
     userInputButton.hidden=true; 
     audioOriginal.play()
     pontos+=20
     score.innerText=pontos   
   }
-    else if (inputCounter.length<=3 && input.toUpperCase() !== song) { //if se a resposta esta errada mas ainda tem vida
-      
-      if (inputCounter.length === 3) { //na terceira tentativa 
+    else if (inputCounter.length<=3 && input.toUpperCase() !== song) { //if condition for wrong answer but still have chance to try 
+      if (inputCounter.length === 3) { //if condition for third attempt 
         userInputButton.hidden=true; 
         outputAnswer.innerText=`Sorry! You guessed wrong, the song is ${song}`
         audioOriginal.play() 
@@ -133,44 +113,17 @@ userInputButton.addEventListener('click', () => {
         score.innerText=pontos   
         return
       }
-
       outputAnswer.innerText="Try it again!"
       setTimeout(() => {
       outputAnswer.innerText=""
       },1500);
     }
   userInput.value=""
-
   if (pontos<=0) {
     gameOver()
   }
-
 });  
   
-function reset () { //seta tudo para a proxima rodada
-  inputCounter=[]
-  outputAnswer.innerText=""
-  userInput.value=""
-  userInputButton.hidden=false
-}
 
-function gameOver() {
-  inputDiv.classList.add("hidden")
-  gameOverScreen.classList.remove("hidden")
-}
 
-function winGame() {
-  inputDiv.classList.add("hidden")
-  winScreen.classList.remove("hidden")
-}
-console.log(inputDiv)
     
-
-
-// function playMusic(event) {
-//     for (let i = 0; i < musics.length; i++) {
-//         Audio.play.hint1(musics)
-//     }
- 
-// alert("cliquei no botao")
-//   console.log("cliquei no botao") 
